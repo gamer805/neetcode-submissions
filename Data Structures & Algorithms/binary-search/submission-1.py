ff1ac3f3df3c6fc84dc72a9b2caad7360e1004a9
Jude Downing <40494198+gamer805@users.noreply.github.com>
2026-06-18 09:53:12 -0600

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        remainder = list(enumerate(nums))
+        while len(remainder) > 1:
+            middle = int(len(remainder) / 2)
+            print(middle)
+            if remainder[middle][1] == target:
+                return remainder[middle][0]
+            elif remainder[middle][1] > target:
+                remainder = remainder[:middle]
+            else:
+                remainder = remainder[middle:]
+        
+        if remainder and remainder[0][1] == target:
+            return remainder[0][0]
+        
+        return -1
+        
